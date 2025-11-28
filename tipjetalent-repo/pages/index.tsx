75462067366719e530a56c154fc483e7ed6fe908
anoukwigchert <anouk.wigchert@gmail.com>
2025-11-28 13:44:16 +0100

    Delete tipjetalent-repo directory

--- tipjetalent-repo/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Hallo wereld 👋</h1>
-      <p>Welkom op de landingspagina van Tip Je Talent.</p>
-    </div>
-  );
-}
