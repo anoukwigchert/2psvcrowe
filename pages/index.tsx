3169493cd40713b1a807ee6fdc537326cb2dadf9
anoukwigchert <anouk.wigchert@gmail.com>
2025-11-28 13:45:39 +0100

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Hallo wereld 👋</h1>
+      <p>Welkom op de landingspagina van Tip Je Talent.</p>
+    </div>
+  );
+}
